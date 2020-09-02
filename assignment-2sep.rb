59dbc0106d4847b2f9bb295fdcb94b6168f6caa1
sonam <sonam.saxena1591@gmail.com>
2020-09-03 00:31:31 +0530

    assignment1-2sep

---
+++ assignment-2sep.rb
@@ -0,0 +1,20 @@
+def odd_even(number)
+
+	# if number % 2 == 0
+
+	# 	puts "I m even"
+
+	# else
+	# 	puts "I m odd"	
+				
+ # 	end
+ 	if number.even?
+ 		puts "i m even"
+
+ 	else
+ 		puts "i m odd"
+ 	end	
+
+end
+
+odd_even(19)
